4db0ccf24f211e21e9f57aa7cb20b7f331f46274
Shakthi-lakshmanan-2006 <sakthilakshman521@gmail.com>
2026-01-17 15:11:22 +0530

    Inital part of landing page

---
+++ index.js
@@ -0,0 +1,40 @@
+const roles = [
+  "Computer Science Student.",
+  "Cloud Enthusiast.",
+  "Creative Problem Solver."
+];
+
+const typingText = document.getElementById("typing-text");
+
+let roleIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typingSpeed = 100;
+const deletingSpeed = 60;
+const holdTime = 2000; // 2 seconds
+
+function typeEffect() {
+  const currentRole = roles[roleIndex];
+
+  if (!isDeleting) {
+    typingText.textContent = currentRole.substring(0, charIndex + 1);
+    charIndex++;
+
+    if (charIndex === currentRole.length) {
+      setTimeout(() => isDeleting = true, holdTime);
+    }
+  } else {
+    typingText.textContent = currentRole.substring(0, charIndex - 1);
+    charIndex--;
+
+    if (charIndex === 0) {
+      isDeleting = false;
+      roleIndex = (roleIndex + 1) % roles.length;
+    }
+  }
+
+  setTimeout(typeEffect, isDeleting ? deletingSpeed : typingSpeed);
+}
+
+typeEffect();
